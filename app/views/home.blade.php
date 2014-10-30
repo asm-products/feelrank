51bb404de30a11b8c508f6b9f1c124b1f9c18081
Josh Quintal <joshquin@gmail.com>
2014-10-30 17:08:22 -0400

    Added container to home page.

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -8,33 +8,32 @@
 		<p class="jumbotron-small">or <a href="users/login">login</a></p>
 	</div>
 
-	<div class="row marketing">
-		<div class="col-sm-10 col-sm-offset-1">
-			<div class="row">
-				<h3 class="text-center">Here's the jist:</h3>
+	<div class="container">
+		<div class="row marketing">
+			<div class="col-sm-10 col-sm-offset-1">
+				<div class="row">
+					<h3 class="text-center">Here's the jist:</h3>
 
-				<div class="col-md-4">
-					<h1 class="text-center h1-icon"><i class="fa fa-laptop"></i></h1>
-					<h4>Post a Link</h4>
-					<p>Anywhere. Anything! If it's got a url, it's got a new home on FeelRank.</p>
-				</div>
+					<div class="col-md-4">
+						<h1 class="text-center h1-icon"><i class="fa fa-laptop"></i></h1>
+						<h4>Post a Link</h4>
+						<p>Anywhere. Anything! If it's got a url, it's got a new home on FeelRank.</p>
+					</div>
 
-				<div class="col-md-4">
-					<h1 class="text-center h1-icon"><i class="fa fa-comment-o"></i></h1>
-					<h4>Add Your Thoughts</h4>
-					<p>Love it? Hate it? Why? FeelRank is a live collection of your thoughts and feelings.</p>
-				</div>
+					<div class="col-md-4">
+						<h1 class="text-center h1-icon"><i class="fa fa-comment-o"></i></h1>
+						<h4>Add Your Thoughts</h4>
+						<p>Love it? Hate it? Why? FeelRank is a live collection of your thoughts and feelings.</p>
+					</div>
 
-				<div class="col-md-4">
-					<h1 class="text-center h1-icon"><i class="fa fa-line-chart"></i></h1>
-					<h4>Change Your Mind? Change Your Rank.</h4>
-					<p>Maybe that thing wasn't so cool--let us know! The ranks and discussions never stop changing.</p>
+					<div class="col-md-4">
+						<h1 class="text-center h1-icon"><i class="fa fa-line-chart"></i></h1>
+						<h4>Change Your Mind? Change Your Rank.</h4>
+						<p>Maybe that thing wasn't so cool--let us know! The ranks and discussions never stop changing.</p>
+					</div>
 				</div>
 			</div>
 		</div>
 	</div>
 
-	<br />
-	<br />
-	<br />
 @stop
