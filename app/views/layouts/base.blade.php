e5285a09789beaa4f71b7f8e94b71aa26e36b63d
Josh Quintal <joshquin@gmail.com>
2014-11-06 13:40:35 -0500

    Added mobile sharing bar, more styling.

--- app/views/layouts/base.blade.php
+++ app/views/layouts/base.blade.php
@@ -260,6 +260,9 @@
     <!-- Go to www.addthis.com/dashboard to customize your tools -->
     <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-545ba8747c10300f" async="async"></script>
 
+    <!-- Go to www.addthis.com/dashboard to customize your tools -->
+    <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-545ba8747c10300f" async="async"></script>
+
     @yield('more_js')
 
   </body>
