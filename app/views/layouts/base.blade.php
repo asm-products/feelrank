74b92cc01c435dbb94576c85ed1e2030fd2c236e
Josh Quintal <joshquin@gmail.com>
2014-11-13 17:25:00 -0500

    Added Google Analytics.

--- app/views/layouts/base.blade.php
+++ app/views/layouts/base.blade.php
@@ -22,6 +22,17 @@
     <![endif]-->
 
     <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-545ba8747c10300f"></script>
+
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-56442823-2', 'auto');
+      ga('send', 'pageview');
+
+    </script>
   </head>
 
   <body>
