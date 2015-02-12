3d6817889559951d33fcc456f1785fc5d3e7f94f
viciousambitious <joshquin@gmail.com>
2015-02-12 15:39:51 +0000

    Tweaked beta email.

--- app/views/emails/beta.blade.php
+++ app/views/emails/beta.blade.php
@@ -5,8 +5,8 @@
 <p style="text-align: center;">Thank you for signing up for our mailing list! We'll be in touch once the beta is open.</p>
 
 <p style="text-align: center;">
-    Thanks again,<br />
-    <img src="{{ asset('img/feelrank-logo-email.png') }}" alt="FeelRank" />
+    Thanks again,<br /><br />
+    <img style="width: 120px; height: auto;" src="{{ asset('img/signature-small.gif') }}" alt="FeelRank" /><br />
     <strong>Josh Quintal</strong><br />
     Founder
 </p>
